503f35be60f14fd511ecc9143da36610f5a8278b
iguana499 <goxaniguana@gmail.com>
2021-11-01 22:14:19 +0400

    lab2

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
 import pandas
+import eli5
+from tree import Manager
+
 
 data = pandas.read_csv("titanic.csv")
 
-count = 0
-for i in range(len(data["Pclass"])):
-    if (data["Pclass"][i]) == 1 and  (data["Sex"][i]) == "male":
-        count = count + 1
 
-print(count)
+''' --Keys--
+0 - PassengerId
+1 - Survived
+2 - Pclass
+3 - Name
+4 - Sex
+5 - Age
+6 - SibSp
+7 - Ticket
+8 - Fare
+9 - Cabin
+10 - Embarked
+'''
+keys = data.keys()
+
+
+manager = Manager(data)
+
+data = manager.data_preparation()
+
+# отделение нужных данных
+X = data.drop([keys[0], keys[1], keys[2], keys[3], keys[4], keys[6], keys[7], keys[8], keys[9]], axis=1)
+y = data[keys[1]]
+print(X.head())
+# выборка данных для обучения и тестирования
+X_train = X[:-200]
+X_test = X[-200:]
+y_train = y[:-200]
+y_test = y[-200:]
+
+clf = manager.learn_tree(X_train,y_train)
+manager.test_tree(X_train,y_train,X_test,y_test)
+
+# отпределение веса
+print(eli5.explain_weights_sklearn(clf, feature_names=X_train.columns.values))
