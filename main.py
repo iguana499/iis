aca2f72720ff10b74acc6f1aa3ebbf9d3864c00f
iguana499 <40342696+iguana499@users.noreply.github.com>
2021-11-18 19:17:55 +0400

    lab2_v2

--- main.py
+++ main.py
@@ -2,42 +2,44 @@ import pandas
 import eli5
 from tree import Manager
 
-
 data = pandas.read_csv("titanic.csv")
 
-
-''' --Keys--
-0 - PassengerId
-1 - Survived
-2 - Pclass
-3 - Name
-4 - Sex
-5 - Age
-6 - SibSp
-7 - Ticket
-8 - Fare
-9 - Cabin
-10 - Embarked
-'''
-keys = data.keys()
+PASSENGER_ID = "PassengerId"
+SURVIVED = "Survived"
+PASSENGER_CLASS = "Pclass"
+NAME = "Name"
+SEX = "Sex"
+AGE = "Age"
+SIBSP = "SibSp"
+PARCH = "Parch"
+TICKET = "Ticket"
+FARE = "Fare"
+CABIN = "Cabin"
+EMBARKED = "Embarked"
 
 
-manager = Manager(data)
 
-data = manager.data_preparation()
 
-# отделение нужных данных
-X = data.drop([keys[0], keys[1], keys[2], keys[3], keys[4], keys[6], keys[7], keys[8], keys[9]], axis=1)
-y = data[keys[1]]
-print(X.head())
-# выборка данных для обучения и тестирования
-X_train = X[:-200]
-X_test = X[-200:]
-y_train = y[:-200]
-y_test = y[-200:]
+def work(data):
+    manager = Manager(data)
 
-clf = manager.learn_tree(X_train,y_train)
-manager.test_tree(X_train,y_train,X_test,y_test)
+    data = manager.data_preparation()
 
+    # отделение нужных данных
+    X = data.drop([PASSENGER_ID, SURVIVED, PASSENGER_CLASS, NAME, SEX, SIBSP, PARCH, TICKET, FARE], axis=1)
+    y = data[SURVIVED]
+    print(X.head())
+    # выборка данных для обучения и тестирования
+    X_train = X[:-200]
+    X_test = X[-200:]
+    y_train = y[:-200]
+    y_test = y[-200:]
+
+    clf = manager.learn_tree(X_train, y_train)
+    manager.test_tree(X_train, y_train, X_test, y_test)
+    return [clf, X_train]
+
+
+result = work(data)
 # отпределение веса
-print(eli5.explain_weights_sklearn(clf, feature_names=X_train.columns.values))
+print(eli5.explain_weights_sklearn(result[0], feature_names=result[1].columns.values))
