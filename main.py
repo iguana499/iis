3c00db7d23614915da0dd727d1a69326dd8a07ac
iguana499 <40342696+iguana499@users.noreply.github.com>
2021-11-18 18:48:51 +0400

    lab1_v2

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import pandas
 
 data = pandas.read_csv("titanic.csv")
 
-count = 0
-for i in range(len(data["Pclass"])):
-    if (data["Pclass"][i]) == 1 and  (data["Sex"][i]) == "male":
-        count = count + 1
+CLASS_NUMBER = 1
+PASSENGER_CLASS = "Pclass"
+SEX = "Sex"
 
-print(count)
+
+def count_first_class_man():
+    count = 0
+    for i in range(len(data[PASSENGER_CLASS])):
+        if (data[PASSENGER_CLASS][i]) == CLASS_NUMBER and (data[SEX][i]) == "male":
+            count = count + 1
+    return count
+
+
+print(count_first_class_man())
