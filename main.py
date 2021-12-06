b07b6c4529deec122e17447e22ed6ad400b28b89
iguana499 <40342696+iguana499@users.noreply.github.com>
2021-12-06 21:22:31 +0400

    lab3

--- main.py
+++ main.py
@@ -1,45 +1,17 @@
-import pandas
-import eli5
-from tree import Manager
-
-data = pandas.read_csv("titanic.csv")
-
-PASSENGER_ID = "PassengerId"
-SURVIVED = "Survived"
-PASSENGER_CLASS = "Pclass"
-NAME = "Name"
-SEX = "Sex"
-AGE = "Age"
-SIBSP = "SibSp"
-PARCH = "Parch"
-TICKET = "Ticket"
-FARE = "Fare"
-CABIN = "Cabin"
-EMBARKED = "Embarked"
+import numpy as np
+from sklearn.datasets import make_classification
+from models import Models
 
 
+data = make_classification(n_samples=500, n_features=2, n_redundant=0, n_informative=2, random_state=1, n_clusters_per_class=1)
 
+X = np.array(data[0])
+y = np.array(data[1])
 
-def work(data):
-    manager = Manager(data)
+X = X[:, np.newaxis, 1]
 
-    data = manager.data_preparation()
+manager = Models(X, y)
 
-    # отделение нужных данных
-    X = data.drop([PASSENGER_ID, SURVIVED, PASSENGER_CLASS, NAME, SEX, SIBSP, PARCH, TICKET, FARE], axis=1)
-    y = data[SURVIVED]
-    print(X.head())
-    # выборка данных для обучения и тестирования
-    X_train = X[:-200]
-    X_test = X[-200:]
-    y_train = y[:-200]
-    y_test = y[-200:]
-
-    clf = manager.learn_tree(X_train, y_train)
-    manager.test_tree(X_train, y_train, X_test, y_test)
-    return [clf, X_train]
-
-
-result = work(data)
-# отпределение веса
-print(eli5.explain_weights_sklearn(result[0], feature_names=result[1].columns.values))
+manager.linear()
+manager.perceptron()
+manager.ridge()
